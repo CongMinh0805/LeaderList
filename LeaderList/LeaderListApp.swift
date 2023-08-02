8b7c1d52944bb9d2e438854d9e658d4f1c5e110b
CongMinh0805 <dangcongminh2002@gmail.com>
2023-08-02 11:16:42 +0700

    Initial Commit

---
+++ LeaderList/LeaderListApp.swift
@@ -0,0 +1,17 @@
+//
+//  LeaderListApp.swift
+//  LeaderList
+//
+//  Created by Minh Dang Cong on 02/08/2023.
+//
+
+import SwiftUI
+
+@main
+struct LeaderListApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
